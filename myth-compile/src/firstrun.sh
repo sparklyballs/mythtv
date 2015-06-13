3befe89f8b3cfad57d29cc56ce7c29551ed5fd05
sparklyballs <sparklyballs@gmail.com>
2015-06-13 23:47:19 +0100

    refried beans, or just a clean up ?

---
+++ myth-compile/src/firstrun.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [[ $(cat /etc/timezone) != $TZ ]] ; then
+  echo "$TZ" > /etc/timezone
+  dpkg-reconfigure -f noninteractive tzdata
+fi
+
+mkdir  /var/run/sshd
+mkdir  /root/.vnc
+/usr/bin/supervisord -c /root/supervisord.conf
+while [ 1 ]; do
+/bin/bash
+done
