f76fb738a7c05d710ab8a60204c210d4f5e07107
sparklyballs <sparklyballs@gmail.com>
2015-06-13 10:34:47 +0100

    trying integrated database again

--- mythtv/src/002-bring-up-rdp.sh
+++ mythtv/src/002-bring-up-rdp.sh
@@ -2,7 +2,4 @@
 mkdir -p  /var/run/sshd
 mkdir  -p /root/.vnc
 chown -R mythtv:users /home/mythtv/
-/usr/bin/supervisord -c /root/supervisor-files/rdp-supervisord.conf
-while [ 1 ]; do
-/bin/bash
-done
+/usr/bin/supervisord -c /root/supervisor-files/rdp-supervisord.conf & >/dev/null 2>&1
---
+++ mythtv/src/003-bring-up-database.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+start_mysql(){
+    /usr/bin/mysqld_safe --datadir=/config/databases > /dev/null 2>&1 &
+    RET=1
+    while [[ RET -ne 0 ]]; do
+        mysql -uroot -e "status" > /dev/null 2>&1
+        RET=$?
+        sleep 1
+    done
+}
+
+# If databases do not exist create them
+if [ -f /config/databases/mysql/user.MYD ]; then
+  echo "Database exists."
+else
+  echo "Creating database."
+  /usr/bin/mysql_install_db --datadir=/db >/dev/null 2>&1
+  start_mysql
+  echo "Database created. Granting access to 'mythtv' ruser for all hosts."
+  mysql -uroot -e "CREATE USER 'mythtv' IDENTIFIED by 'mythtv'" 
+  mysql -uroot -e "GRANT ALL PRIVILEGES ON *.* TO 'mythtv'@'%' WITH GRANT OPTION"
+  mysql -uroot -e "CREATE DATABASE mythconverg"
+  mysqladmin -u root shutdown
+  chown -R nobody:users /db
+  chmod -R 755 /db
+fi
+
+echo "Starting MariaDB..."
+/usr/bin/mysqld_safe --skip-syslog --datadir='/db'
+
+
+
