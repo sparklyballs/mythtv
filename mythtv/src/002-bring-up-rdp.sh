47c5fa702ac41d1cda3a6885680934fe418b4ef9
sparklyballs <sparklyballs@gmail.com>
2015-06-13 01:07:16 +0100

    Update 002-bring-up-rdp.sh

--- mythtv/src/002-bring-up-rdp.sh
+++ mythtv/src/002-bring-up-rdp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir -p  /var/run/sshd
 mkdir  -p /root/.vnc
-chown -R ubuntu:users /home/ubuntu/
+chown -R mythtv:users /home/mythtv/
 /usr/bin/supervisord -c /root/supervisor-files/rdp-supervisord.conf
 while [ 1 ]; do
 /bin/bash
